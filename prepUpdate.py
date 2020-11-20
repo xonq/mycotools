1a2eea395a7b3b5d561942f773c22657810beb05
xonq <konkelzach@protonmail.com>
2020-11-20 14:23:19 -0500

    changed functionality to work with importing databases

--- prepUpdate.py
+++ prepUpdate.py
@@ -18,6 +18,24 @@ if __name__ == '__main__':
 	if not os.path.isdir( sys.argv[2] ):
 		os.mkdir( sys.argv[2] )
 
+	names, new_files = [], []
 	for file in files:
 		subprocess.Popen( ['cp', file, sys.argv[2]], stdout = subprocess.PIPE, \
 			stderr = subprocess.PIPE )
+		name = os.path.basename( file )
+		names.append( name.replace( '.py', '' ) )
+		new_files.append( os.path.abspath( sys.argv[2]  ) + '/' + name )
+
+	for file in new_files:
+		with open( file, 'r' ) as raw:
+			data = raw.read()
+		for name in names:
+			data1 = re.sub( r'^import(.*?)' + name + r'(.*?)', r'import\1kontools.' + name + r'\2', data )
+			data2 = re.sub( r'^from(.*?)' + name + r'(.*?)', r'from\1kontools.' + name + r'\2', data1 )
+			if data2 != data:
+				with open( file, 'w' ) as out:
+					out.write( data2 )
+
+	sys.exit( 0 )
+			
+
