d2978757be00249325400b3e1c0b56b418c029fd
xonq <konkelzach@protonmail.com>
2020-11-20 14:24:09 -0500

    moved to correct folder

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open( "README.md", "r" ) as fh:
 	long_description = fh.read()
 
 setuptools.setup(
-	name = "kontools-xonq",
-	version = "0.0.1",
+	name = "kontools",
+	version = "0.0.3",
 	author = "xonq",
 	author_email = "konkelzach@protonmail.com",
 	description = "A compilation of bioinformatic and computation biology inspired " + \
@@ -13,11 +13,11 @@ setuptools.setup(
 	long_description = long_description,
 	long_description_content_type = "text/markdown",
 	url = "https://gitlab.com/xonq/kontools/kontools",
-	packages = setuptools.find_packages(),
+	packages = setuptools.find_packages( ),
 	classifiers = [
 		"Programming Language :: Python :: 3",
 		"License :: OSI Approved :: MIT License",
-		"Operating System :: Linux / Unix",
+		"Operating System :: POSIX :: Linux",
 	],
 	python_requires = '>=3.0',
 )
