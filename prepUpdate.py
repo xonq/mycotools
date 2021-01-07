7228a4fe7677f8bd2088fd95d2c09a0dec07c5d2
xonq <konkelzach@protonmail.com>
2021-01-07 11:56:57 -0500

    cmon

--- prepUpdate.py
+++ prepUpdate.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3
 
-import subprocess, sys, os, glob, re
+import subprocess, sys, os, glob, re, argparse
 
 def collect_files(directory = './', filetype = '*', recursive = True, verbose = False):
 
@@ -25,25 +25,51 @@ def collect_files(directory = './', filetype = '*', recursive = True, verbose = 
     return filelist
 
 
+def changeVersion( setup_path, version ):
+
+    with open( setup_path, 'r' ) as raw:
+        data = raw.read()
+
+    new_data = re.sub( r'version = .*', 'version = "' + version + '",', data, re.M )
+    with open( setup_path, 'w' ) as out:
+        out.write( new_data )
+
 
 if __name__ == '__main__':
 
-	usage = 'Input update directory of files and output for package, prepares update'
-	if '-h' in sys.argv or '--help' in sys.argv:
-		print( '\n' + usage + '\n' )
-		sys.exit( 1 )
-	elif len( sys.argv ) < 3:
-		print( '\n' + usage + '\n' )
-		sys.exit( 2 )
+    parser = argparse.ArgumentParser( description = 'Prepares mycotools update for pip' )
+    parser.add_argument( '-i', '--input', help = 'Input directory' )
+    parser.add_argument( '-o', '--output', help = 'Output directory' )
+    parser.add_argument( '-v', '--version', help = 'Version update setup.py' )
+    args = parser.parse_args()
 
-	files = collect_files( sys.argv[1], 'py' )
-	if not os.path.isdir( sys.argv[2] ):
-		os.mkdir( sys.argv[2] )
+    files = collect_files( args.input, 'py' )
+    if not os.path.isdir( args.output ):
+        os.mkdir( args.output )
 
-	for file in files:
-		subprocess.Popen( ['cp', file, sys.argv[2]], stdout = subprocess.PIPE, \
-			stderr = subprocess.PIPE )
+    for file in files:
+        subprocess.Popen( ['cp', file, args.output], stdout = subprocess.PIPE, \
+            stderr = subprocess.PIPE )
 
-	sys.exit( 0 )
-			
+    prevOutput = os.path.dirname( os.path.abspath( args.output ) ) + '/'
+    if not os.path.isdir( prevOutput + 'archive' ):
+        os.mkdir( prevOutput + 'archive/' )
+    os.chdir( prevOutput )
+    delOld = subprocess.call( ['rm', '-rf', '*dist*', os.path.basename(os.path.abspath(args.output)) + \
+        '.egg_info'], stdout = subprocess.PIPE, stderr = subprocess.PIPE, shell = True )
+    if args.version:
+        changeVersion( prevOutput + 'setup.py', args.version )
+
+    setup = subprocess.call(
+        'python3 ' + prevOutput + 'setup.py sdist bdist_wheel',
+        shell = True
+    )
+    if setup == 0: 
+        upload = subprocess.call(
+            'python3 -m twine upload --repository pypi dist/*',
+            shell = True
+        )
+
+    sys.exit( 0 )
+            
 
--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ with open( "README.md", "r" ) as fh:
 	long_description = fh.read()
 
 setuptools.setup(
-	name = "kontools",
-	version = "0.0.3",
+	name = "mycotools",
+	version = "0.0.7a",
 	author = "xonq",
 	author_email = "konkelzach@protonmail.com",
 	description = "A compilation of bioinformatic and computation biology inspired " + \
 		"python tools",
 	long_description = long_description,
 	long_description_content_type = "text/markdown",
-	url = "https://gitlab.com/xonq/kontools/kontools",
+	url = "https://gitlab.com/xonq/mycotools/mycotools_pypi",
 	packages = setuptools.find_packages( ),
 	classifiers = [
 		"Programming Language :: Python :: 3",
