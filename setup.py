681b68f86ae2338928e1662e789aab98db1cce16
xonq <konkelzach@protonmail.com>
2021-01-22 18:34:04 -0500

    "0.3.1b"

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open( "README.md", "r" ) as fh:
 
 setuptools.setup(
     name = "mycotools",
-    version = "0.3.0b5",
+    version = "0.3.1b",
     author = "xonq",
     author_email = "konkelzach@protonmail.com",
     description = "A compilation of bioinformatic and computation biology inspired " + \
