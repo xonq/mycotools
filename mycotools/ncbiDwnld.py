8226fad1dde9a3b0785dc576cd0860c6b90174d9
xonq <konkelzach@protonmail.com>
2024-07-15 10:42:14 -0700

    fix 404 on MD5 download

--- mycotools/ncbiDwnld.py
+++ mycotools/ncbiDwnld.py
@@ -303,10 +303,14 @@ def collect_ftps(
             if dwnld == 0:
                 with open(output_path + '.tmpmd5', 'r') as raw:
                     for line in raw:
-#                        data = line.rstrip().split('  ')
-                        data = line.rstrip().split()
-                        if data:
-                            md5s[ftp_path + '/' + os.path.basename(data[1])] = data[0]
+                        data = line.rstrip().split('  ')
+#                        data = line.rstrip().split()
+                        if data and len(data) == 2:
+                            try:
+                                md5s[ftp_path + '/' + os.path.basename(data[1])] = data[0]
+                            except IndexError: # 404 error or something else
+                                md5s = {}
+                                break
             else:
                 md5s = {}
 
